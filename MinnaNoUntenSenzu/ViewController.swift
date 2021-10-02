b83962758ced8c08282a4e4864d51aec524eae1c
iyu.swiftLeaner <iyu@live.jp>
2021-10-02 20:36:30 +0900

    Initial Commit

---
+++ MinnaNoUntenSenzu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinnaNoUntenSenzu
+//
+//  Created by 岩渕優児 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
